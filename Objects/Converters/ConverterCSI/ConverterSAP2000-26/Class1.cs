38d5f4525b766abb3bc5dd0193e688150c6e8f2b
Hadi Aram Bashir - Admin <adm-hmbashir@darbeirut.com>
2025-11-03 12:53:37 +0200

    Added reference to SAP 26 converter in connector

--- Objects/Converters/ConverterCSI/ConverterSAP2000-26/Class1.cs
+++ Objects/Converters/ConverterCSI/ConverterSAP2000-26/Class1.cs
@@ -3,7 +3,7 @@ using Speckle.Core.Kits;
 
 namespace Objects.Converter.SAP2000;
 
-public class ConverterSAP200026 : ConverterCSI, ISpeckleConverter
+public class ConverterSAP2000 : ConverterCSI, ISpeckleConverter
 {
   // Empty class: this is just to expose the shared converter to the loader.
 }
